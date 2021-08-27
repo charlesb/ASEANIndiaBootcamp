79074c24bf51b74ca23a79bfbf33720961067a25
bernardcharles@gmail.com <bernardcharles@gmail.com>
2021-08-27 00:24:00 +0000

    Switch back to db_sensor_iot_db

--- Instructor/SQLA-PREP.py
+++ Instructor/SQLA-PREP.py
@@ -11,16 +11,20 @@
 # MAGIC %scala
 # MAGIC spark.conf.set("com.databricks.training.module_name", "Sensor_IoT")
 # MAGIC val dbNamePrefix = {
-# MAGIC   val tags = com.databricks.logging.AttributionContext.current.tags
-# MAGIC   val name = tags.getOrElse(com.databricks.logging.BaseTagDefinitions.TAG_USER, java.util.UUID.randomUUID.toString.replace("-", ""))
-# MAGIC   val username = if (name != "unknown") name else dbutils.widgets.get("databricksUsername")
+# MAGIC //   val tags = com.databricks.logging.AttributionContext.current.tags
+# MAGIC //   val name = tags.getOrElse(com.databricks.logging.BaseTagDefinitions.TAG_USER, java.util.UUID.randomUUID.toString.replace("-", ""))
+# MAGIC //   val username = if (name != "unknown") name else dbutils.widgets.get("databricksUsername")
 # MAGIC   
-# MAGIC   val username_final = username.split('@')(0)
-# MAGIC   val module_name = spark.conf.get("com.databricks.training.module_name").toLowerCase()
+# MAGIC //   val username_final = username.split('@')(0)
+# MAGIC //   val module_name = spark.conf.get("com.databricks.training.module_name").toLowerCase()
 # MAGIC 
-# MAGIC   val databaseName = (username_final+"_"+module_name).replaceAll("[^a-zA-Z0-9]", "_") + "_db"
-# MAGIC   spark.conf.set("com.databricks.training.spark.dbName", databaseName)
-# MAGIC   spark.conf.set("com.databricks.training.spark.userName", username_final)
+# MAGIC //   val databaseName = (username_final+"_"+module_name).replaceAll("[^a-zA-Z0-9]", "_") + "_db"
+# MAGIC //   spark.conf.set("com.databricks.training.spark.dbName", databaseName)
+# MAGIC //   spark.conf.set("com.databricks.training.spark.userName", username_final)
+# MAGIC   
+# MAGIC   // Set unique DB for all users
+# MAGIC   spark.conf.set("com.databricks.training.spark.dbName", "db_sensor_iot_db")
+# MAGIC   spark.conf.set("com.databricks.training.spark.userName", "db")
 # MAGIC }
 
 # COMMAND ----------
